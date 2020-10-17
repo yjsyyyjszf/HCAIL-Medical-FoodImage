e6f551c4d68b2483b5c9ec5f3b5310de2d3a70af
Daria-SubinPark <57142322+Daria-SubinPark@users.noreply.github.com>
2020-10-18 03:42:11 +0900

    Provided body-parser extended option in urlencoded

--- medicalserver/server.js
+++ medicalserver/server.js
@@ -16,7 +16,8 @@ app.use(express.json({
     limit: "50mb"
 }))
 app.use(express.urlencoded({
-    limit: "50mb"
+    limit: "50mb",
+    extended : false,
 }))
 app.use(bodyParser.json({limit: '50mb'}))
 app.use(bodyParser.urlencoded({limit:'50mb', extended: true, parameterLimit:true }))
